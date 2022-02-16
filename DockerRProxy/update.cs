eb32c5ada93e42f5a0987f5d280943150574e3ca
Primož Ajdišek <ajdisek.primoz@gmail.com>
2022-02-16 01:47:34 +0000

    working stuff

--- DockerRProxy/update.cs
+++ DockerRProxy/update.cs
@@ -6,22 +6,90 @@ using Yarp.ReverseProxy.Model;
 
 namespace DockerRProxy;
 
-static class update{
+static class update
+{
     public static InMemoryConfigProvider x;
 
     public static void runUpdate(List<RouteConfig> o, List<ClusterConfig> i)
     {
         List<RouteConfig> a = new();
         List<ClusterConfig> b = new();
-
+        int number = 0;
         DockerClient client = new DockerClientConfiguration().CreateClient();
         var containerinfo = client.Containers.ListContainersAsync(new ContainersListParameters()).Result;
-        foreach(var c in containerinfo)
+        foreach (var c in containerinfo)
         {
-            Console.WriteLine(c.Names[0]);
+            if (c.Ports.Count >= 1)
+            {
+                foreach (Port p in c.Ports)
+                {
+                    if (p.PublicPort == 80)
+                    {
+                        a.Add(new RouteConfig()
+                        {
+                            RouteId = number.ToString(),
+                            ClusterId = number.ToString(),
+                            Match = new RouteMatch
+                            {
+                                Hosts = new List<string>() { $"{c.Names[0].Replace("/", "")}.bigpod.si" }
+
+                            }
+                        });
+
+                        b.Add(new ClusterConfig()
+                        {
+                            ClusterId = number.ToString(),
+                            SessionAffinity = new SessionAffinityConfig { Enabled = true, Policy = "Cookie", AffinityKeyName = ".Yarp.ReverseProxy.Affinity" },
+                            Destinations = new Dictionary<string, DestinationConfig>(StringComparer.OrdinalIgnoreCase)
+                            {
+                                {
+                                    "destination1", new DestinationConfig() { Address = $"http://{c.Names[0].Replace("/", "")}" }
+                                },
+                            }
+                        });
+
+                        number++;
+                        Console.WriteLine(c.Names[0]);
+                    }
+                    else if(p.PublicPort == 443)
+                    {
+                        a.Add(new RouteConfig()
+                        {
+                            RouteId = number.ToString(),
+                            ClusterId = number.ToString(),
+                            Match = new RouteMatch
+                            {
+                                Hosts = new List<string>() { $"{c.Names[0].Replace("/", "")}.bigpod.si" }
+
+                            }
+                        });
+
+                        b.Add(new ClusterConfig()
+                        {
+                            ClusterId = number.ToString(),
+                            SessionAffinity = new SessionAffinityConfig { Enabled = true, Policy = "Cookie", AffinityKeyName = ".Yarp.ReverseProxy.Affinity" },
+                            Destinations = new Dictionary<string, DestinationConfig>(StringComparer.OrdinalIgnoreCase)
+                            {
+                                {
+                                    "destination1", new DestinationConfig() { Address = $"http://{c.Names[0].Replace("/", "")}" }
+                                },
+                            }
+                        });
+
+                        number++;
+                        Console.WriteLine(c.Names[0]);
+                    }
+                }
+            }
         }
 
 
-        x.Update(a,b);
+        x.Update(a, b);
+
+        foreach (var item in a)
+            Console.WriteLine(item.ClusterId);
+        {
+
+        }
     }
 }
